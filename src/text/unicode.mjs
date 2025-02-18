ab2fcdad3853148332f3ac5b4064ec874c6de41a
Karl Lew <karl@firepick.org>
2025-02-18 06:59:59 -0800

    v2.15.0 += LINUX_COLOR

--- src/text/unicode.mjs
+++ src/text/unicode.mjs
@@ -261,6 +261,14 @@ export class Unicode {
       MAGENTA: '\u001b[35m',
       CYAN: '\u001b[36m',
       WHITE: '\u001b[37m',
+      BRIGHT_BLACK: '\u001b[90m',
+      BRIGHT_RED: '\u001b[91m',
+      BRIGHT_GREEN: '\u001b[92m',
+      BRIGHT_YELLOW: '\u001b[93m',
+      BRIGHT_BLUE: '\u001b[94m',
+      BRIGHT_MAGENTA: '\u001b[95m',
+      BRIGHT_CYAN: '\u001b[96m',
+      BRIGHT_WHITE: '\u001b[97m',
       NO_COLOR: '\u001b[0m',
     };
   }
